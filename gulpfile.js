371401d731d5f6c69f97138a3de87c4e9da39768
PJ <69799563+pj-web@users.noreply.github.com>
2024-02-07 14:53:45 +0400

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require("gulp");
 const nunjucksRender = require("gulp-nunjucks-render");
+const replace = require('gulp-replace');
+// const { src, dest } = require('gulp');
 
 gulp.task("default", function () {
   return gulp.src("src/emails/*").pipe(nunjucksRender({
     path: ["src/templates"]
-  })).pipe(gulp.dest("dist"));
+  })).pipe(gulp.dest("dist")).pipe(replace('/{/{', '{{')).pipe(gulp.dest("dist"));
 });
+
+// gulp.task("default", function replaceQuotes() {
+//   return gulp.src("dist/*.html")
+//     .pipe(replace('/{/{', '{{'))
+//     .pipe(gulp.dest("dist"));
+// });
